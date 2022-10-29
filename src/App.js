836bae750c29fb56543bf6b46d1653fe0f2aa4c9
DevGit21 <94892636+DevGit21@users.noreply.github.com>
2022-10-29 23:46:25 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,80 +1,73 @@
 import React from 'react';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState,useRef  } from "react";
 import './style.css';
 
-export default function App() {  
-  const imageURL =   'https://utopiamusic.com/logo.png';
+function App() {
+  const imageURL = 'https://utopiamusic.com/logo.png';
   const [jsonData, setData] = useState([]);
   const [continentList, setContinentData] = useState([]);
-  const [countryList, setContryData] = useState([]);
-  const componentRef = useRef(null);
-  //const [clicked, setClicked] = useState(false);
+  const [countryList, setcountryData] = useState([]);
+  const componentRef = useRef(null); // for get div element
   const fetchData = () => {
     fetch(`https://api.countries.code-test.utopiamusic.com/all`)
       .then((response) => response.json())
       .then((actualData) => {
         setData(actualData);
+        // get all continents
         const key = 'continent';
-        const arrayOfContinents = [
-          ...new Map(actualData.map((item) => [item[key], item])).values(),
-        ];
-
-        setContinentData(arrayOfContinents);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+        const arrayOfcontinents = [...new Map(actualData.map(item =>
+          [item[key], item])).values()];  
+          setContinentData(arrayOfcontinents);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  const onSerializeData = (contient, e) => {
-    Array.from(e.target.parentElement.children).forEach(function (item, i) {
-      item.className = '';
+  const showContryList = (continent,e) => {
+    Array.from(e.target.parentElement.children).forEach(function(item,i) {
+      item.className = ""; //remove active class from othere element
     });
-    e.target.classList.add('active');
+    e.target.classList.add('active');     
 
-    const filtered = jsonData.filter((data) => {
-      return data.continent === contient;
+    //get country list by continent
+    const filtered = jsonData.filter(data => {
+      return data.continent === continent;
     });
-    setContryData(filtered);
-
-    componentRef.current.scrollTo(0, 0);
+    setcountryData(filtered);
+    componentRef.current.scrollTo(0, 0);    
   };
 
-  const setClicked = (e) => {
-    Array.from(e.target.parentElement.children).forEach(function (item, i) {
-      item.className = '';
+  const highlightCountry = (e) => {    
+    Array.from(e.target.parentElement.children).forEach(function(item,i) {
+      item.className = ""; //remove active class from othere element
     });
-    e.target.classList.add('active');
+    e.target.classList.add('active');    
   };
 
   return (
     <div className="App">
       <header className="App-header">
-      <img src={imageURL} />
+        <img src={imageURL} className="Utopia-logo" alt="logo" />
       </header>
       <div className="content">
         <div className="continent-name">
-          {continentList.map((value) => {
+          {continentList.map(value => {
             return (
-              <a
-                key={value.continent}
-                onClick={(e) => onSerializeData(value.continent, e)}
-              >
+              <a key={value.continent} onClick={(e) => showContryList(value.continent,e)}>
                 {value.continent}
               </a>
             );
           })}
         </div>
         <div className="country-list" ref={componentRef}>
-          {countryList.map((country) => {
+          {countryList.map(country => {
             return (
-              <a key={country.alpha2Code} onClick={(e) => setClicked(e)}>
-                {country.name}
-              </a>
+                <a key={country.alpha2Code} onClick={ (e) => highlightCountry(e) }>{country.name}</a>              
             );
           })}
         </div>
@@ -82,3 +75,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
